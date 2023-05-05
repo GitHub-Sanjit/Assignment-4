3918c4c38904a8884c68633a9e3e499c09610d12
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-05-05 17:21:13 +0600

    Has Return + Parameter

---
+++ 4.c
@@ -0,0 +1,12 @@
+// Has Return + Parameter
+#include<stdio.h>
+
+int multiply(int x,int y){
+    return x * y;
+}
+int main()
+{
+    int x = 2, y = 3;
+    int res = multiply(x, y);
+    return 0;
+}
